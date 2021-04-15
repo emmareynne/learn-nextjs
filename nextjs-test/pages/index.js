c8324d3c93b56dc43f6875a9ad44c6eba5839990
Emma Reynne <erdesilva25@gmail.com>
2021-04-15 10:54:38 -0500

    adding link to index

--- nextjs-test/pages/index.js
+++ nextjs-test/pages/index.js
@@ -3,6 +3,7 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import { getHugoPostData } from '../lib/hugosite'
+import Link from 'next/link'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -38,6 +39,10 @@ export default function Home({ allPostsData }) {
               {id}
               <br />
               {date}
+              <br />
+              <Link href={`/posts/${id}`}>
+                <a>Link</a>
+              </Link>
             </li>
           ))}
         </ul>
